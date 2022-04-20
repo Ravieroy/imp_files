2c4f057538757675053be2c0fc5271c2fc1ef402
Ravi Roy <raviroy@pop-os.localdomain>
2022-04-20 13:47:30 +0530

    added folder

---
+++ p4vasp/python-packages/p4vasp/config.py
@@ -0,0 +1,6 @@
+p4vasp_home='/home/shubham/p4vasp'
+name       ="p4vasp"
+version    ="0.3.30"
+release    ="1"
+build_date ="October 06 2015"
+
---
+++ p4vasp/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -Rf /home/shubham/p4vasp/python-packages/p4vasp
+rm -Rf /home/shubham/p4vasp/python-packages/cp4vasp.py
+rm -Rf /home/shubham/p4vasp/python-packages/_cp4vasp.so
+rm -f  /home/shubham/p4vasp/bin/p4v.py
+rm -f  /home/shubham/p4vasp/bin/p4v
+rm -Rf /home/shubham/p4vasp/include/ODP
+rm -Rf /home/shubham/p4vasp/include/p4vasp
+rm -Rf /home/shubham/p4vasp/lib/libp4vasp.a 
+rm -Rf /home/shubham/p4vasp/lib/libODP.a
+rm -Rf /home/shubham/p4vasp
